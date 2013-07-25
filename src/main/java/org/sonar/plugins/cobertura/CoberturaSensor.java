14b4a30a32f6f74d85d98265c66962a0d51d5fab
Julien HENRY <julien.henry@sonarsource.com>
2013-07-25 10:45:47 +0200

    Decrease dependency on Sonar 3.6 and raise dependency to Sonar Java 1.4

--- src/main/java/org/sonar/plugins/cobertura/CoberturaSensor.java
+++ src/main/java/org/sonar/plugins/cobertura/CoberturaSensor.java
@@ -29,8 +29,7 @@ import org.sonar.api.resources.Project;
 import org.sonar.api.resources.Resource;
 import org.sonar.api.scan.filesystem.ModuleFileSystem;
 import org.sonar.api.scan.filesystem.PathResolver;
-import org.sonar.api.utils.CoberturaReportParserUtils;
-import org.sonar.api.utils.CoberturaReportParserUtils.FileResolver;
+import org.sonar.plugins.cobertura.CoberturaReportParserUtils.FileResolver;
 import org.sonar.plugins.cobertura.base.CoberturaConstants;
 
 import java.io.File;
